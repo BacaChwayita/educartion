fbb68e4f0528a44c18f41c798b15c4a9d10f8eda
Nian Vrey <51823073+YoungVoid@users.noreply.github.com>
2026-04-28 17:15:17 +0200

    Add test for Register function

---
+++ backend/service/auth/auth_test.go
@@ -0,0 +1,21 @@
+package auth_test
+
+import (
+	"testing"
+
+	"github.com/P-SEN371-Group-3/educartion/model"
+)
+
+func TestRegister(t *testing.T) {
+	user := model.RegisterRequest{
+		Full_name:     "Test Name",
+		Email:         "test@testmail.com",
+		Password_text: "MyPassword123!",
+	}
+
+	err := Register(user)
+
+	if err != nil {
+		t.Errorf("Expected nil, got %s", err.Error())
+	}
+}
