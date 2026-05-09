a9a0c697b79bb4e0e435f73e39e6b79b7c5d141f
Nian Vrey <51823073+YoungVoid@users.noreply.github.com>
2026-05-09 13:17:46 +0200

    Fixed scripts to drop first then create

--- data/scripts/powershell/setup_db.ps1
+++ data/scripts/powershell/setup_db.ps1
@@ -1,5 +1,22 @@
 Write-Host "Starting DB Setup Scripts..."
 
+# Drop tables
+Write-Host "Drop Tables"
+docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "DROP TABLE IF EXISTS app.cart_item CASCADE;"
+Write-Host "Drop Test Tables"
+docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "DROP TABLE IF EXISTS app.cart CASCADE;"
+
+Write-Host "Drop Test Tables"
+docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "DROP TABLE IF EXISTS app_test.cart_item CASCADE;"
+Write-Host "Drop Test Tables"
+docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "DROP TABLE IF EXISTS app_test.cart CASCADE;"
+
+# Confirm tables dropped
+Write-Host "Check Tables"
+docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "\dt app.*"
+Write-Host "Check Test Tables"
+docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "\dt app_test.*"
+
 # Create DB + Schemas
 Write-Host "Create DB"
 Get-Content .\scripts\database\001_create_database.sql | docker exec -i educartion-postgres psql -U postgres -d postgres
@@ -13,23 +30,6 @@ Write-Host "Create Test Tables"
 Get-Content .\scripts\schema\002_create_tables_app_test.sql | docker exec -i educartion-postgres psql -U postgres -d educartion_db
 
 # Check tables created
-Write-Host "Check Tables"
-docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "\dt app.*"
-Write-Host "Check Test Tables"
-docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "\dt app_test.*"
-
-# Drop tables
-Write-Host "Drop Tables"
-docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "DROP TABLE IF EXISTS app.cart_item CASCADE;"
-Write-Host "Drop Test Tables"
-docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "DROP TABLE IF EXISTS app.cart CASCADE;"
-
-Write-Host "Drop Test Tables"
-docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "DROP TABLE IF EXISTS app_test.cart_item CASCADE;"
-Write-Host "Drop Test Tables"
-docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "DROP TABLE IF EXISTS app_test.cart CASCADE;"
-
-# Confirm tables dropped
 Write-Host "Check Tables"
 docker exec -it educartion-postgres psql -U postgres -d educartion_db -c "\dt app.*"
 Write-Host "Check Test Tables"
