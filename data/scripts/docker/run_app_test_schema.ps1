0b3909228a6394162101f9513f0d832d59098b35
YoungVoid <51823073+YoungVoid@users.noreply.github.com>
2026-05-29 17:45:42 +0200

    Refactor data directory

--- data/scripts/docker/run_app_test_schema.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$composeFile = "data\scripts\docker\docker-compose.postgres.yml"
-
-docker compose -f $composeFile up -d postgres
-docker compose -f $composeFile exec -T postgres psql -U postgres -d postgres -f /sql/database/001_create_database.sql
-docker compose -f $composeFile exec -T postgres psql -U postgres -d educartion_db -f /sql/database/002_create_schemas.sql
-docker compose -f $composeFile exec -T postgres psql -U postgres -d educartion_db -f /sql/schema/002_create_tables_app_test.sql
-
