3b44a563e636624d6f81e9d66c0c665e4e5692e2
YoungVoid <51823073+YoungVoid@users.noreply.github.com>
2026-05-29 17:58:21 +0200

    Add test db and fix up docker files

---
+++ data/init/001_create_test_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Create test db. Main db made by default by postgres image.
+createdb -U "$POSTGRES_USER" "$POSTGRES_DB_TEST"
+
+# Create tables in both dbs.
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /sql/data/scripts/create_tables.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_TEST" -f /sql/data/scripts/create_tables.sql
