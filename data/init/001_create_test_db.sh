90a057029ca72f4dfeada61c7089c98adb87fee2
YoungVoid <51823073+YoungVoid@users.noreply.github.com>
2026-05-30 12:03:02 +0200

    feat(data): Dummy data seed file added

--- data/init/001_create_test_db.sh
+++ data/init/001_create_test_db.sh
@@ -7,3 +7,6 @@ createdb -U "$POSTGRES_USER" "$POSTGRES_DB_TEST"
 # Create tables in both dbs.
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /sql/data/scripts/create_tables.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_TEST" -f /sql/data/scripts/create_tables.sql
+
+# Create Dummy Data
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /sql/data/scripts/seed/001_seed_data.sql
